d0692516e3b867bd26ee18c21c01556d099e909c
Thais-MW <thais.wermelinger@aluno.cefet-rj.br>
2022-12-28 09:42:26 -0300

    Adiciona projeto

---
+++ P2__Saulo_Thais/main.c
@@ -0,0 +1,11 @@
+//Projeto para P2 De Programação Estrutura
+// Feito por Saulo Klein e Thaís Munier
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Arquivo inicial");
+    return 0;
+}
